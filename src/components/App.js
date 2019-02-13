99cf363996eea7b7b5c71806c4fcafc0c3c75620
Conrad Wright <easp.conrad@gmail.com>
2019-02-13 08:34:03 -0700

    /testing react-redux tutorial

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CommentBox from 'components/CommentBox.js'
+import CommentList from 'components/CommentList.js';
+
+export default () => {
+    return (
+        <div>
+            <CommentBox />
+            <CommentList />
+        </div>
+    )
+}
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import CommentBox from 'components/CommentBox'
+import { mount } from 'enzyme'
+import Root from '../../Root'
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = mount(
+        <Root>
+            <CommentBox />
+        </Root>)
+})
+
+afterEach(() => {
+    wrapper.unmount()
+})
+
+it('displays text area and submit button', () => {
+    expect(wrapper.find('textarea').length).toEqual(1)
+    expect(wrapper.find('button').length).toEqual(1)
+}) 
+
+describe('the text area', () => {
+    beforeEach(() => {
+        wrapper.find('textarea').simulate('change', {
+            target: { value: 'new comment' }
+        })
+        wrapper.update()
+    })
+    
+    it('has textarea user can type in', () => {
+        expect(wrapper.find('textarea').prop('value')).toEqual('new comment')
+    })
+
+    it('clears text area after submit', () => {
+        wrapper.find('form').simulate('submit')
+        wrapper.update()
+        expect(wrapper.find('textarea').prop('value')).toEqual('')
+    })
+})
